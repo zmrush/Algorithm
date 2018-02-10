605e296d76ef606145fa193ed4e8557bc4ec0a87
zmrush <zmhust88@gmail.com>
2018-02-10 23:05:31 +0800

    forever young

--- src/main/java/ForeverYoung.java
+++ src/main/java/ForeverYoung.java
@@ -4,6 +4,30 @@ import java.util.Scanner;
  * Created by mingzhu7 on 2018/2/9.
  */
 public class ForeverYoung {
+    public static long getHigher(long y,char[] s){
+        int length=s.length;
+        long low=(long)Math.ceil(Math.pow((Math.ceil((y+0.0)/(s[0]-'0'))),1.0/(length-1)));
+        while(true){
+            long addtmp=1;
+            long y0=0;
+            long tmp;
+            boolean stop=true;
+            for(int i=0;i<length;i++){
+                tmp=(s[length-i-1]-'0')*addtmp;
+                if(y0>=(Long.MAX_VALUE-tmp)) {
+                    stop=false;
+                    break;
+                }
+                y0+=tmp;
+                addtmp*=low;
+            }
+            if(stop && y0<=y){
+                break;
+            }
+            low--;
+        }
+        return low;
+    }
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         long y=scanner.nextLong();long l=scanner.nextLong();
@@ -29,28 +53,48 @@ public class ForeverYoung {
             s="100";
         }
         length=s.length();
+        if(length<=3){
+            long a=s.charAt(0)-'0';
+            long b=s.charAt(1)-'0';
+            char[] buf=new char[3];
+            buf[0]=s.charAt(0);buf[1]=s.charAt(1);
+            for(int c=s.charAt(2)-'0';c<=9;c++){
+                buf[2]=(char)(c+'0');
+                long higher=getHigher(y,buf);
+                if((a*higher*higher+b*higher+c)==(y)){
+                    System.out.println(higher);
+                    return;
+                }
+            }
+            for(b=s.charAt(1)-'0'+1;b<=9;b++){
+                for(int c=0;c<=9;c++){
+                    buf[1]=(char)(b+'0');buf[2]=(char)(c+'0');
+                    long higher=getHigher(y,buf);
+                    if((a*higher*higher+b*higher+c)==(y)){
+                        System.out.println(higher);
+                        return;
+                    }
+                }
+            }
+            for(a=s.charAt(0)-'0'+1;a<=9;a++){
+                for(b=0;b<=9;b++){
+                    for(int c=0;c<=9;c++){
+                        buf[0]=(char)(a+'0');buf[1]=(char)(b+'0');buf[2]=(char)(c+'0');
+                        long higher=getHigher(y,buf);
+                        if((a*higher*higher+b*higher+c)==(y)){
+                            System.out.println(higher);
+                            return;
+                        }
+                    }
+                }
+            }
+            s="1000";
+        }
         //brute-force
         //search the biggest x;then brute-force
-        long low=(long)Math.ceil(Math.pow((y/(s.charAt(0)-'0')),1.0/(s.length()-1)));
-        while(true){
-            long addtmp=1;
-            long y0=0;
-            long tmp;
-            boolean stop=true;
-            for(int i=0;i<length;i++){
-                tmp=(s.charAt(length-i-1)-'0')*addtmp;
-                if(y0>=(Long.MAX_VALUE-tmp)) {
-                    stop=false;
-                    break;
-                }
-                y0+=tmp;
-                addtmp*=low;
-            }
-            if(stop && y0<=y){
-                break;
-            }
-            low--;
-        }
+        //------------------------------------------------------------------------------
+        long low=getHigher(y,s.toCharArray());
+        //-----------------------------------------------------------------------------------------
         for(;;low--){
             long y0=y;
             long remainder;
